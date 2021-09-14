f5c9560e08386f55d0d64eb6c79d5e184023a648
khorh <helen.khor@googlemail.com>
2021-09-14 15:01:08 +0100

    created header and ticket tracker component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
-import './App.scss';
+import "./App.scss";
+
+import TicketTracker from "./components/TicketTracker/TicketTracker";
+
+import team from "./data/team";
 
 function App() {
   return (
     <div className="App">
+      <header>
+          <h1>Ticket Tracker</h1>
+      </header>
+      <main>
+
+      </main>
     </div>
   );
 }
---
+++ src/components/TicketTracker/TicketTracker.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TicketTracker.scss";
+
+const TicketTracker = () => {
+    return (
+        <section>
+
+        </section>
+    );
+};
+
+export default TicketTracker;
