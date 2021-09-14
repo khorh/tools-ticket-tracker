0689d96b2a20c17b1939026248979b5d57f720f4
Reshmy Devaraju <reshmyd@gmail.com>
2021-09-14 16:50:41 +0100

    updated scss and created ticket component

--- src/components/TicketTracker/TicketTracker.jsx
+++ src/components/TicketTracker/TicketTracker.jsx
@@ -1,27 +1,11 @@
-import React, { useState } from "react";
+
 import "./TicketTracker.scss";
 
+import Ticket from "../Ticket/Ticket"
+
 const TicketTracker = ({teamArr}) => {
-    const [counter, setCounter] = useState(0);
-
-    const counterIncrement = (id) => setCounter(counter + 1);
-
-    const counterDecrement = (id) => counter === 0 ? setCounter(0) : setCounter(counter - 1);
-
-    const ticketJsx = teamArr.map(elem => {
-        return (
-          <div>
-            <h3>{elem.name}</h3>
-            <h4>{elem.role}</h4>
-            <div data-id={elem.id}>
-              <h4>Counter</h4>
-              <h5>{counter}</h5>
-                <button onClick={() => counterDecrement(elem.id)}>-</button>
-                <button onClick={counterIncrement}>+</button>
-            </div>
-          </div>
-        );
-      });
+    
+    const ticketJsx = teamArr.map(elem => <Ticket elem={elem} />);
 
     return (
         <section className="ticket-container">
