4ffc59605c6f225bbbc07dcbec8ff957f86802a6
khorh <helen.khor@googlemail.com>
2021-09-14 16:09:38 +0100

    updated state functionality

--- src/components/TicketTracker/TicketTracker.jsx
+++ src/components/TicketTracker/TicketTracker.jsx
@@ -4,19 +4,19 @@ import "./TicketTracker.scss";
 const TicketTracker = ({teamArr}) => {
     const [counter, setCounter] = useState(0);
 
-    const counterIncrement = () => setCounter(counter + 1);
+    const counterIncrement = (id) => setCounter(counter + 1);
 
-    const counterDecrement = () => counter === 0 ? setCounter(0) : setCounter(counter - 1);
+    const counterDecrement = (id) => counter === 0 ? setCounter(0) : setCounter(counter - 1);
 
     const ticketJsx = teamArr.map(elem => {
         return (
           <div>
             <h3>{elem.name}</h3>
             <h4>{elem.role}</h4>
-            <div>
+            <div data-id={elem.id}>
               <h4>Counter</h4>
               <h5>{counter}</h5>
-                <button onClick={counterDecrement}>-</button>
+                <button onClick={() => counterDecrement(elem.id)}>-</button>
                 <button onClick={counterIncrement}>+</button>
             </div>
           </div>
